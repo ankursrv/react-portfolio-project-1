1e88cc6ea20dd855fab715f8d024ce3bc3d1c51c
ankur srivastava <ankur.srivastava@techchefz.com>
2024-08-29 12:05:17 +0530

    nav item active

--- src/component/Header/index.jsx
+++ src/component/Header/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import BannerContent from '../BannerContent'
 import Button from '../Button'
 import { FaArrowUp } from "react-icons/fa6";
@@ -21,6 +22,7 @@ const navItems = [
 ]
 
 const Header = () => {
+    const [active, setActive] = useState(navItems[0].name)
     return (
         <section className='bg-my-gradient h-screen relative'>
             <img src="/images/vector-background.png" alt="" className='absolute opacity-60' />
@@ -34,7 +36,7 @@ const Header = () => {
                     <ul className='md:flex hidden text-white text-lg space-x-5'>
                         {
                             navItems.map((item) => (
-                                <li key={item.id} className="px-[5px] relative z-20 transition-all hover:shadow-shadow-tertary hover:text-black-primary duration-500 cursor-pointer">{item.name}</li>
+                                <li key={item.id} className={`px-[5px] relative z-20 transition-all hover:shadow-shadow-tertary hover:text-black-primary duration-500 cursor-pointer ${active === item.name ? 'active' : ''}`} onClick={() => setActive(item.name)} >{item.name}</li>
                             ))
                         }
                     </ul>
